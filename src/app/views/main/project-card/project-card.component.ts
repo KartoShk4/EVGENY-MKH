93d3b729d277fce1c1a5bf5d3d7faf1cd34956ff
Evgeny_MKH <trisemerka@gmail.com>
2025-05-04 20:54:37 +0500

    Заполнены все карточки

--- src/app/views/main/project-card/project-card.component.ts
+++ src/app/views/main/project-card/project-card.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input} from '@angular/core';
 import {ProjectCardType} from "../../../types/project-card.type";
 
 @Component({
@@ -6,41 +6,59 @@ import {ProjectCardType} from "../../../types/project-card.type";
   templateUrl: './project-card.component.html',
   styleUrls: ['./project-card.component.scss']
 })
-export class ProjectCardComponent implements OnInit {
+export class ProjectCardComponent {
 
   @Input() project!: ProjectCardType;
 
 
-  constructor() {
-  }
-
-  ngOnInit(): void {
-  }
-
   projects: ProjectCardType[] = [
     {
       imageUrl: '../assets/images/pages/card/cookies.png',
-      title: 'Проект "Macaroons"',
-      description: 'Верстка одностраничного сайта. Страница с товарами, ценами и формой обратной связи.',
+      title: 'COOKIES',
+      description: 'Уютный лендинг с домашним печеньем, описанием продукции, процессом приготовления и отзывами. Адаптивный дизайн.',
+      technologies: 'HTML, SCSS, TypeScript, Angular',
+      siteLink: 'https://kartoshk4.github.io/cookies/',
+      codeLink: 'https://github.com/KartoShk4/cookies'
+    },
+    {
+      imageUrl: '../assets/images/pages/card/burgers.png',
+      title: 'BURGER HOUSE',
+      description: 'Стильный лендинг бургерной с меню, акциями и отзывами. Адаптивный, с ярким дизайном и удобной навигацией.',
+      technologies: 'HTML, SCSS, Angular, TypeScript',
+      siteLink: 'https://kartoshk4.github.io/barbershop/',
+      codeLink: 'https://github.com/KartoShk4/barbershop'
+    },
+    {
+      imageUrl: '../assets/images/pages/card/macaroons.png',
+      title: 'MACAROONS',
+      description: 'Лендинг для продажи макарунов с описанием видов, преимуществами и отзывами. Адаптивный и стильный дизайн.',
       technologies: 'HTML, Less, JavaScript',
       siteLink: 'https://kartoshk4.github.io/macaroons/',
       codeLink: 'https://github.com/KartoShk4/macaroons'
     },
     {
-      imageUrl: '../assets/images/pages/card/cookies.png',
-      title: 'Проект "Macaroons"',
-      description: 'Верстка одностраничного сайта. Страница с товарами, ценами и формой обратной связи.',
-      technologies: 'HTML, Less, JavaScript',
-      siteLink: 'https://kartoshk4.github.io/macaroons/',
-      codeLink: 'https://github.com/KartoShk4/macaroons'
+      imageUrl: '../assets/images/pages/card/barbershop.jpg',
+      title: 'BARBERSHOP',
+      description: 'Современный одностраничный сайт с акцентом на стильный мужской дизайн. Содержит информацию об услугах, преимуществах, ценах и мастерах.',
+      technologies: 'HTML, SCSS, JavaScript',
+      siteLink: 'https://kartoshk4.github.io/barbershop/',
+      codeLink: 'https://github.com/KartoShk4/barbershop'
     },
     {
-      imageUrl: '../assets/images/pages/card/cookies.png',
-      title: 'Проект "Macaroons"',
-      description: 'Верстка одностраничного сайта. Страница с товарами, ценами и формой обратной связи.',
-      technologies: 'HTML, Less, JavaScript',
-      siteLink: 'https://kartoshk4.github.io/macaroons/',
-      codeLink: 'https://github.com/KartoShk4/macaroons'
+      imageUrl: '../assets/images/pages/card/pizza.jpg',
+      title: 'PIZZA CHEFF',
+      description: 'Лендинг пиццерии с меню, акциями и отзывами. Яркий дизайн, адаптивная верстка и удобная навигация.',
+      technologies: 'HTML, CSS, JavaScript',
+      siteLink: 'https://kartoshk4.github.io/pizza/',
+      codeLink: 'https://github.com/KartoShk4/pizza'
+    },
+    {
+      imageUrl: '../assets/images/pages/card/kappadokia.png',
+      title: 'KAPPADOKIA',
+      description: 'Лендинг о Каппадокии с описанием достопримечательностей, туров и фото. Адаптивный и красочный дизайн.',
+      technologies: 'HTML, CSS',
+      siteLink: 'https://kartoshk4.github.io/kappadokia/',
+      codeLink: 'https://github.com/KartoShk4/kappadokia'
     },
   ]
 }
