a4cad5751a2c233dbee4dadc52da845173416338
Evgeny_MKH <trisemerka@gmail.com>
2025-05-16 15:22:34 +0500

    Адаптация под экран 768(добавление мобильного меню)

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,14 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, HostListener} from '@angular/core';
 
 @Component({
   selector: 'header-component',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent {
 
-  constructor() { }
+  menuOpen: boolean = false;
 
-  ngOnInit(): void {
+  toggleMenu(): void {
+    this.menuOpen = !this.menuOpen;
+    // Блокируем/разблокируем скролл
+    if (this.menuOpen) {
+      document.body.classList.add('lock');
+    } else {
+      document.body.classList.remove('lock');
+    }
+  }
+
+  // Закрываем меню при клике на ссылку (для мобильной версии)
+  closeMenu(): void {
+    this.menuOpen = false;
+    document.body.classList.remove('lock');
+  }
+
+  // Закрываем меню при изменении размера экрана (если стало больше 768px)
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any): void {
+    if (window.innerWidth > 768) {
+      this.menuOpen = false;
+      document.body.classList.remove('lock');
+    }
   }
 }
+
+
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,9 +3,9 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { RouterLinkWithHref } from "@angular/router";
-import {TagColorPipe} from "./pipes/tagColorPipe";
-import {TypedDirective} from "./directives/typed.directive";
-import {TiltDirective} from "./directives/tilt.directive";
+import { TagColorPipe } from "./pipes/tagColorPipe";
+import { TypedDirective } from "./directives/typed.directive";
+import { TiltDirective } from "./directives/tilt.directive";
 
 
 
