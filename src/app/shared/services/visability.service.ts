37a6ed634c504d966cf1efa308eea7e8d5b40315
Evgeny_MKH <trisemerka@gmail.com>
2025-05-18 11:44:33 +0500

    Применение переменных для всех цветов в проекте

--- src/app/shared/services/visability.service.ts
+++ src/app/shared/services/visability.service.ts
@@ -1,15 +1,15 @@
 // visibility.service.ts
 import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
+import {BehaviorSubject, Observable} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class VisibilityService {
-  private showHeaderFooter = new BehaviorSubject<boolean>(true);
-  currentVisibility = this.showHeaderFooter.asObservable();
+  private showHeaderFooter: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(true);
+  currentVisibility: Observable<boolean> = this.showHeaderFooter.asObservable();
 
-  changeVisibility(visible: boolean) {
+  changeVisibility(visible: boolean): void {
     this.showHeaderFooter.next(visible);
   }
 }
